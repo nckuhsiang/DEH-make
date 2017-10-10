8e342abb4ea21a24bf3d54ea3b7f530ef0adcb0d
Ray Chen <raychen@Ray-MacBook-Pro.local>
2017-10-10 20:30:51 +0800

    Initial Commit

---
+++ UItest1010/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UItest1010
+//
+//  Created by Ray Chen on 2017/10/10.
+//  Copyright © 2017年 Ray Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
